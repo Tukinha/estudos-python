6c632578f5e967d7de7ac8feb6d53f0a1603af8d
Tukinha <arthur.almeida27@hotmail.com>
2023-08-02 17:04:36 -0300

    cálculo da área de um círculo

---
+++ atividade5.py
@@ -0,0 +1,4 @@
+'''Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.'''
+raio = float(input("Digite o raio da circunferencia: "))
+area = 3.14*raio**2
+print(f"A área da circunferência é: {area}")
