0c699f01024eca59355055ab891e9ac63a28e420
Tukinha <arthur.almeida27@hotmail.com>
2023-08-02 16:42:38 -0300

    notas e médias

---
+++ atividade3.py
@@ -0,0 +1,7 @@
+'''Faça um Programa que peça as 4 notas bimestrais e mostre a média.'''
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+nota4 = float(input("Digite a quarta nota: "))
+media = (nota1+nota2+nota3+nota4)/3
+print(f'A sua média é: {media:.1f}')
