373472bf6fcc766aeb11cd4c02d61f5836cf263e
Tukinha <arthur.almeida27@hotmail.com>
2023-08-02 16:55:37 -0300

    conversão de metros para centimetros

---
+++ atividade4.py
@@ -0,0 +1,4 @@
+'''Faça um Programa que converta metros para centímetros.'''
+metro = float(input("Digite a metragem para conversão: "))
+calculo = (metro*100)
+print(f"O retorno da conversão foi: {calculo}")
