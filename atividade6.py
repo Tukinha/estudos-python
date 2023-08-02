a7fab316cc8c70ff6370e982a9cf0695835bcc23
Tukinha <arthur.almeida27@hotmail.com>
2023-08-02 17:17:31 -0300

    Calculo da area do circulo

---
+++ atividade6.py
@@ -0,0 +1,5 @@
+'''Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário'''
+lado = float(input("Digite o valor de um dos lados do quadrado: "))
+area = (lado*lado)**2
+print(f"O resultado do cálculo do dobro da área é: {area}")
+
