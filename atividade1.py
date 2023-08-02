2d0baab7e5158aebedbf1a0d52be3942b3464e26
Tukinha <arthur.almeida27@hotmail.com>
2023-08-02 15:11:35 -0300

    primeiro commit

---
+++ atividade1.py
@@ -0,0 +1,3 @@
+#Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número]
+primeiroNumero = int(input("Digite um número: "))
+print(f"O número digitado foi: {primeiroNumero} ")
